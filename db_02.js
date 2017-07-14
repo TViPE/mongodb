e885532a21e72de2d1dffda208bffd38af2f75f2
TRI VI <trivi2710@gmail.com>
2017-07-14 17:44:52 -0400

    add find (find, findOne, findById) and querying

--- db_02.js
+++ db_02.js
@@ -46,18 +46,59 @@ var User = mongoose.model('user', userSchema);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
 
-var chris = new User ({
-	name: 'Chris',
-	username: 'new_selilayha',
-	password: 'new_password'
-});
+//------------ CREAT ------------ //
+// var chris = new User ({
+// 	name: 'Chris',
+// 	username: 'new_selilayha',
+// 	password: 'new_password'
+// });
 
-chris.dudify(function (err, name){
-	if(err) throw err;
-	console.log('Your name is ' + name);
-});
+// chris.dudify(function (err, name){
+// 	if(err) throw err;
+// 	console.log('Your name is ' + name);
+// });
 
-chris.save(function(err){
-	if(err) throw err;
-	console.log('User save succefully');
-})
+// chris.save(function(err){
+// 	if(err) throw err;
+// 	console.log('User save succefully');
+// })
+
+// var newUser = User({
+// 	name: 'Peter Quill',
+// 	username: 'starlord55',
+// 	password: 'password',
+// 	admin: true
+// });
+
+// newUser.save(function(err){
+// 	if(err) {
+// 		console.log(err);
+// 	}
+// 	console.log('User created!');
+// })
+
+//------------ Read ------------ //
+
+// User.find({username: 'starlord55'}, function(err, data){
+// 	if(err){
+// 		throw err;
+// 	}
+// 	console.log(data);
+// })
+
+// User.findById(1, function(err, data) {
+// 	if (err) {
+// 		throw err;
+// 	}
+// 	console.log(data);
+// })
+
+//------------ Querying ------------ //
+
+// var monthAgo = new Date();
+// monthAgo.setMonth(monthAgo.getMonth() -1);
+
+// User.find({admin: true}).where('create_at').gt(monthAgo).exec(function (err, users){
+// 	if (err) throw err;
+// 	console.log(users);
+// });
